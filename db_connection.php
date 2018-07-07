3aa2bfe5450896fdf32054d4cf5c75486880663b
Sarthak Aggarwal <sarthaakagarwal@gmail.com>
2018-07-07 20:43:05 +0530

    Add files via upload

--- db_connection.php
+++ db_connection.php
@@ -3,15 +3,15 @@
 <?php
 
 $server="us-cdbr-iron-east-04.cleardb.net";
-$user="bb1b721b07451c";
-$pass="d3441686";
+$user="b08c149bde9abf";
+$pass="8c8e9030";
 $con=mysqli_connect($server,$user,$pass);
 if(!$con)
 {	
 		die("Connection failed:" . $con->connect_error);		
 }
 
-$db=mysqli_select_db($con,"heroku_0b7857f57f33cec");
+$db=mysqli_select_db($con,"heroku_826b63e19883921");
 if(!$db)
 {
 	die("Connection failed: " . $db->connect_error);			
